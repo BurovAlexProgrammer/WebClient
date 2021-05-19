f532164c66911945e76c83d60337ef383b5b6a72
Aleksey <burovalexprogrammer@gmail.com>
2021-05-19 17:09:30 +0500

    Impl router

--- client-portfolio/src/App.tsx
+++ client-portfolio/src/App.tsx
@@ -4,7 +4,7 @@ import AppContext from './contexts/AppContext';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Page_Home } from './components/Pages/Page_Home';
-import { HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   const ctx = useContext(AppContext);
@@ -19,10 +19,12 @@ function App() {
         <link rel='stylesheet' type='text/css' href={ctx.stylePath}/>
         <link rel='stylesheet' type='text/css' href='./styles/toggle.css'/>
         <Header/>
-        
         <main role="main" className="container">
-          <Route exact path="/home" component={Page_Home}/>
-          <Route path="*" component={() => {return <h1>Page not found</h1>;}} />
+          <Switch>
+            <Route exact path="/home" component={Page_Home}/>
+            <Route path="/link" component={() => {return <h1>Link</h1>;}}/>
+            <Route path="/*" component={() => {return <h1>Page not found</h1>;}} />
+          </Switch>
         </main>
         <Footer fixed={true}/>
       </div>
--- client-portfolio/src/components/Header.tsx
+++ client-portfolio/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Navbar, NavLink, Nav} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { ThemeToggle } from './ThemeToggle/ThemeToggle';
 
 export const Header = () => {
@@ -7,8 +8,8 @@ export const Header = () => {
         <Navbar className='bg-primary'>
           <Navbar.Brand href='#home' className='text-light'> BrandTitle </Navbar.Brand>
           <Nav className="mr-auto">
-            <NavLink href='#home' className='text-light'> Home </NavLink>
-            <NavLink href='#link' className='text-light'> Link </NavLink>
+            <NavLink> <Link  className='text-light' to='/home'>Home </Link> </NavLink>
+            <NavLink> <Link  className='text-light' to='/link'>Link </Link> </NavLink>
           </Nav>
           <ThemeToggle/>
         </Navbar>    
