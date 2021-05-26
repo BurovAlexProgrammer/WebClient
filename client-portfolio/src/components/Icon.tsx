201d300dd3003a949436ddd27705745ea4c8c521
Aleksey <burovalexprogrammer@gmail.com>
2021-05-26 19:35:47 +0500

    Feature/home page (#3)
    * Home page filling
    
    * Installed react-router
    Impl router
    
    * Impl router
    
    * Added page NotFound
    
    * Fixed router
    
    * lint cleaning
    
    * Impl portfolio cards
    
    * Added Portfolio content
    
    * Fixed links on header
    added params to portfolio works
    fixed a card style on a dark theme
    Improved Portfolio card
    
    * Temp
    
    * Fixed porttfolio cards
    
    * Added style for icons
    Added few icons
    
    * Impl Portfolio Item "Portfolio site"
    
    * Added icons
    Corrected text color
    Filled Portfolio page - Portfolio Site
    
    * Impl Portfolio Item "Portfolio site"
    
    * Added icons
    Corrected text color
    Filled Portfolio page - Portfolio Site

---
+++ client-portfolio/src/components/Icon.tsx
@@ -0,0 +1,28 @@
+import { Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+type Sizes = 'small' | 'medium' | 'large' | 'custom';
+
+export const Icon = (props: Props & React.HTMLProps<HTMLImageElement>) => {
+    const defSize : Sizes = 'medium';
+    const sizeClass = props.iconSize === 'custom' ? '' : 'pf-icon-'+(props.size || defSize);
+    const highlightClass = props.highligth === undefined || props.highligth ? 'image-hover-highlight' : '';
+    const roundedClass = props.rounded ? 'rounded' : '';
+
+    if (props.iconSize === 'custom' && (!props.width || !props.height)) {
+        throw new Error('Width or height not configured while the size is custom.');
+    }
+    const iconElement = <Image title={props.title} src={props.src} className={`${[props.className, sizeClass, highlightClass, roundedClass].join(' ')}`} width={props.width} height={props.height}/>;
+
+    return props.url ? <Link to={props.url || '#'}> {iconElement} </Link> : iconElement;
+}
+
+
+interface Props {
+    src: string;
+    url?: string;
+    iconSize?: Sizes;
+    rounded?: boolean;
+    highligth?: boolean;
+}
+
